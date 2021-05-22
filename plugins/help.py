0bb4d5ae54db6a1adb09b9cbf4ce985a9ae19d26
akashakash678 <83580768+akashakash678@users.noreply.github.com>
2021-05-22 13:16:47 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,7 +3,11 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"☆ This bot is made by AKASH GV"
-               "☆ You can download all ONLINE CLASS"
-               "☆ Just send me the link copied in MBPU COLLEGE APP"
+    helptxt = """do you need help😁
+    
+1.🔹 <b> I can download the your Online class.</b> \n
+2.🔹 <b> Go to your MBPU COLLEGE APP.</b> \n
+3.🔹 <b> Copy the link of your video.</b> \n
+4.🔹 <b> And paste it here.</b>   \n
+5.🔹 <b> And I'll Upload It Into Telegram </b> \n \n <b>Made With Love By: https://t.me/masterpice38 </b>"""
     await message.reply_text(helptxt)
