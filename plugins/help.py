cc152901c1b356a73b23d21670207dd0a2a60169
akashakash678 <83580768+akashakash678@users.noreply.github.com>
2021-05-22 13:05:11 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,7 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+    helptxt = f"☆ This bot is made by AKASH GV"
+               "☆ You can download all ONLINE CLASS"
+               "☆ Just send me the link copied in MBPU COLLEGE APP"
     await message.reply_text(helptxt)
