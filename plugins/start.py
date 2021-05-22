08a57ecff5b704250d6a15083d7f2118d68187da
akashakash678 <83580768+akashakash678@users.noreply.github.com>
2021-05-22 17:45:44 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,13 +4,17 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 @Client.on_message(Filters.command(["start"]), group=-2)
 async def start(client, message):
     # return
-    joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel" , "Adding soon")],
-        [InlineKeyboardButton("Report Bugs 😊", "Adding soon")]
-        [InlineKeyboardButton("Owner" , url="https://t.me/masterpice38")],
-    
-    
-    ])
+    joinButton = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "Source", url="https://github.com/X-Gorn/X-URL-Uploader"
+                        ),
+                        InlineKeyboardButton("Project Channel", url="https://t.me/xTeamBots"),
+                    ],
+                    [InlineKeyboardButton("Author", url="https://t.me/xgorn")],
+                ]
+            ),
     welcomed = f"Hi <b>{message.from_user.first_name}</b>\nI can download your online class</b>\nIf you don't know how to use me</b>\nThen type /help"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
